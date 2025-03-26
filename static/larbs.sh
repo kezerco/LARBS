453fb54bab466528a1fb8cac7a124666a40b8986
iam kezer <iam@kezer.co>
2025-03-26 14:24:07 +0300

    Update larbs.sh

--- static/larbs.sh
+++ static/larbs.sh
@@ -6,8 +6,8 @@
 
 ### OPTIONS AND VARIABLES ###
 
-dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/static/progs.csv"
+dotfilesrepo="https://github.com/kezerco/dotfiles.git"
+progsfile="https://raw.githubusercontent.com/kezerco/LARBS/blob/master/static/progs.csv"
 aurhelper="yay"
 repobranch="master"
 export TERM=ansi
