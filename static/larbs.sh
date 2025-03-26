b35d47d7ce07d471aca6a939fd5bfd369d345124
iam kezer <iam@kezer.co>
2025-03-26 14:11:25 +0300

    Update larbs.sh

--- static/larbs.sh
+++ static/larbs.sh
@@ -325,21 +325,21 @@ EndSection' >/etc/X11/xorg.conf.d/40-libinput.conf
 
 # All this below to get Librewolf installed with add-ons and non-bad settings.
 
-whiptail --infobox "Setting browser privacy settings and add-ons..." 7 60
+#whiptail --infobox "Setting browser privacy settings and add-ons..." 7 60
 
-browserdir="/home/$name/.librewolf"
-profilesini="$browserdir/profiles.ini"
+#browserdir="/home/$name/.librewolf"
+#profilesini="$browserdir/profiles.ini"
 
 # Start librewolf headless so it generates a profile. Then get that profile in a variable.
-sudo -u "$name" librewolf --headless >/dev/null 2>&1 &
-sleep 1
-profile="$(sed -n "/Default=.*.default-default/ s/.*=//p" "$profilesini")"
-pdir="$browserdir/$profile"
+#sudo -u "$name" librewolf --headless >/dev/null 2>&1 &
+#sleep 1
+#profile="$(sed -n "/Default=.*.default-default/ s/.*=//p" "$profilesini")"
+#pdir="$browserdir/$profile"
 
-[ -d "$pdir" ] && makeuserjs
+#[ -d "$pdir" ] && makeuserjs
 
 # Kill the now unnecessary librewolf instance.
-pkill -u "$name" librewolf
+#pkill -u "$name" librewolf
 
 # Allow wheel users to sudo with password and allow several system commands
 # (like `shutdown` to run without password).
